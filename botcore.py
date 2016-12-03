36b368527e63042dbff7f41cee8fc0ee85e49681
Girai Kaku <kakugirai@gmail.com>
2016-12-03 14:19:06 +0900

    add show registered classes function

--- botcore.py
+++ botcore.py
@@ -3,9 +3,6 @@
 import random
 from selenium import webdriver
 from bs4 import BeautifulSoup
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
 
 URL = "https://wellness.sfc.keio.ac.jp/v3/pc.php"
 USERNAME_XPATH = "/html/body/div/div[2]/div/form/table/tbody/tr[1]/td/input"
@@ -29,67 +26,69 @@ class Bot(object):
     def tear_down(self):
         """close webdriver"""
         self.browser.quit()
+        return
 
     def login(self, username, password, delay=None):
         """login"""
         if delay is None:
             delay = [0.5, 1.5]
-        try:
-            self.browser.get(URL)
-            self.browser.find_element_by_xpath(USERNAME_XPATH).send_keys(username)
-            self.browser.implicitly_wait(random.uniform(delay[0], delay[1]))
-            self.browser.find_element_by_xpath(PASSWORD_XPATH).send_keys(password)
-            self.browser.implicitly_wait(random.uniform(delay[0], delay[1]))
-            self.browser.find_element_by_xpath(SUBMIT_XPATH).click()
-            self.browser.implicitly_wait(random.uniform(delay[0], delay[1]))
-            print("Successfully logged in.")
-            return
-        except Exception as err:
-            raise err
+        self.browser.get(URL)
+        self.browser.find_element_by_xpath(USERNAME_XPATH).send_keys(username)
+        self.browser.implicitly_wait(random.uniform(delay[0], delay[1]))
+        self.browser.find_element_by_xpath(PASSWORD_XPATH).send_keys(password)
+        self.browser.implicitly_wait(random.uniform(delay[0], delay[1]))
+        self.browser.find_element_by_xpath(SUBMIT_XPATH).click()
+        self.browser.implicitly_wait(random.uniform(delay[0], delay[1]))
+        print("Successfully logged in.")
+        return
 
     def show_available_class(self):
         """generate an available class list"""
-        try:
-            self.browser.get(URL)
-            self.browser.find_element_by_xpath(OUTER_ALL_AVAILABLE_CLASSES_XPATH).click()
-            class_list = []
-            available_class_table = BeautifulSoup(
-                self.browser.page_source, 'lxml').find("table", attrs={"class" : "cool"})
-            rows = available_class_table.find_all('tr')
-            for row in rows[1:]:
-                cols = row.find_all("td")
-                class_list.append([cols[i].get_text() for i in range(3)])
-            print(class_list)
-            return
-        except Exception as err:
-            raise err
+        self.browser.get(URL)
+        self.browser.find_element_by_xpath(OUTER_ALL_AVAILABLE_CLASSES_XPATH).click()
+        available_class_list = []
+        available_class_table = BeautifulSoup(
+            self.browser.page_source, 'lxml').find("table", attrs={"class" : "cool"})
+        rows = available_class_table.find_all('tr')
+        for row in rows[1:]:
+            cols = row.find_all("td")
+            available_class_list.append([cols[i].get_text() for i in range(4)] + [cols[5].get_text()])
+        return available_class_list
 
-    def reserve_class(self, date, period, classname, delay=None):
-        """reserve class"""
+    def show_registered_class(self):
+        """generate an reserved classes list"""
+        registered_class_list = []
+        available_class_table = BeautifulSoup(
+            self.browser.page_source, 'lxml').find("table", attrs={"class" : "cool"})
+        rows = available_class_table.find_all('tr')
+        for row in rows[1:]:
+            cols = row.find_all("td")
+            registered_class_list.append([cols[i].get_text() for i in range(4)])
+        return registered_class_list
+
+    def register_class(self, date, period, classname, delay=None):
+        """register class"""
         if delay is None:
             delay = [0.5, 1.5]
-        try:
-            # switch to reservation page
-            self.browser.find_element_by_xpath(RESERVATION_BAR_XPATH).click()
-            self.browser.implicitly_wait(random.uniform(delay[0], delay[1]))
-            # show all available class
-            self.browser.find_element_by_xpath(INNER_ALL_AVAILABLE_CLASSES_XPATH).click()
-            self.browser.implicitly_wait(random.uniform(delay[0], delay[1]))
-            # Choose class
-            class_xpath = '''
-            /html/body/div/div[3]/table[2]/tbody/tr[
-            td[1]/text()[contains(., "{0}")] and 
-            td[2]/text()[contains(., "{1}")] and 
-            td[3]/text()[contains(., "{2}")]]/td[7]/a
-            '''.format(date, period, classname)
-            self.browser.find_element_by_xpath(class_xpath).click()
-            self.browser.implicitly_wait(random.uniform(delay[0], delay[1]))
-            # Reserve class
-            self.browser.find_element_by_xpath(RESERVE_BUTTON_XPATH).click()
-            self.browser.implicitly_wait(random.uniform(delay[0], delay[1]))
-            # Click again
-            self.browser.find_element_by_xpath(RESERVE_BUTTON_XPATH).click()
-            print("You've successfully reserved %s class." % classname)
-            return
-        except Exception as err:
-            raise err
+        # switch to reservation page
+        self.browser.find_element_by_xpath(RESERVATION_BAR_XPATH).click()
+        self.browser.implicitly_wait(random.uniform(delay[0], delay[1]))
+        # get available class list
+        self.browser.find_element_by_xpath(INNER_ALL_AVAILABLE_CLASSES_XPATH).click()
+        self.browser.implicitly_wait(random.uniform(delay[0], delay[1]))
+        # Choose class
+        class_xpath = '''
+        /html/body/div/div[3]/table[2]/tbody/tr[
+        td[1]/text()[contains(., "{0}")] and 
+        td[2]/text()[contains(., "{1}")] and 
+        td[3]/text()[contains(., "{2}")]]/td[7]/a
+        '''.format(date, period, classname)
+        self.browser.find_element_by_xpath(class_xpath).click()
+        self.browser.implicitly_wait(random.uniform(delay[0], delay[1]))
+        # Register class
+        self.browser.find_element_by_xpath(RESERVE_BUTTON_XPATH).click()
+        self.browser.implicitly_wait(random.uniform(delay[0], delay[1]))
+        # Click again
+        self.browser.find_element_by_xpath(RESERVE_BUTTON_XPATH).click()
+        print("You've successfully registered %s class." % classname)
+        return
--- pecli.py
+++ pecli.py
@@ -3,13 +3,14 @@
 import sys
 # import datetime
 from getpass import getpass
+from tabulate import tabulate
 import click
 import botcore
 
 
 if sys.platform == "linux" or sys.platform == "linux2":
     from pyvirtualdisplay import Display
-    # seamless executable on Ubuntu
+    # headless executable on Ubuntu
     DISPLAY = Display(visible=0, size=(800, 600))
     DISPLAY.start()
 elif sys.platform == "darwin":
@@ -24,24 +25,34 @@ def cli():
     pass
 
 @cli.command()
-@click.option('--date', required=True,
-              help="enter the date of the class")
-@click.option('--period', required=True,
-              help="enter the period of the class")
-@click.option('--classname', required=True,
-              help="enter the name of the class")
-def reserve(date, period, classname):
-    """Reserve class"""
+@click.option('--date', '-d', required=True,
+              help="the date of the class")
+@click.option('--period', '-p', required=True,
+              help="the period of the class")
+@click.option('--classname', '-c', required=True,
+              help="the name of the class")
+def register(date, period, classname):
+    """Register class"""
     username = input("CNS ID:")
     password = getpass("CNS Password: ")
     bot = botcore.Bot()
     bot.login(username, password)
-    bot.reserve_class(date, period, classname)
+    bot.register_class(date, period, classname)
     bot.tear_down()
 
 @cli.command()
-def show():
-    """Show available class"""
+@click.option('--all', '-a', is_flag=True,
+              help="show all available classes")
+@click.option('--registered', '-r', is_flag=True,
+              help="show registered classes(login required)")
+def show(all, registered):
+    """Show available classes"""
     bot = botcore.Bot()
-    bot.show_available_class()
+    if all:
+        print(tabulate(bot.show_available_class(), headers=['Date', 'Period', 'Class Name', 'Teacher', 'Available']))
+    if registered:
+        username = input("CNS ID:")
+        password = getpass("CNS Password: ")
+        bot.login(username, password)
+        print(tabulate(bot.show_registered_class(), headers=['Date', 'Period', 'Class Name', 'Teacher']))
     bot.tear_down()
